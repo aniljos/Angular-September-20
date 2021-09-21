2af3a21cce0bfb0d6e64f99daf1fd57e4af77f4d
Anil Joseph <anil.jos@gmail.com>
2021-09-21 12:57:54 +0530

    Day 2

--- awesome-app/src/app/app.module.ts
+++ awesome-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './databinding/databinding.component';
 import { HelloComponent } from './hello/hello.component';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
     AppComponent, HelloComponent, DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- awesome-app/src/app/databinding/databinding.component.ts
+++ awesome-app/src/app/databinding/databinding.component.ts
@@ -7,4 +7,26 @@ import { Component } from "@angular/core";
 })
 export class DataBindingComponent{
 
+    user: {firstName: string, lastName: string};
+    count: number;
+    message: string;
+    age: number = 25;
+    
+
+    constructor(){
+
+        this.user = {firstName: "Anil", lastName: "Joseph"};
+        this.count = 15;
+        this.message = "Hello";
+    }    
+
+    inc(evt: any){
+
+        console.log(evt);
+        this.count++;
+    }
+
+    update(evt: any){
+        this.age = evt.target.value
+    }
 }
